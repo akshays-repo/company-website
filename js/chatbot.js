df1016a4134fe7f06e316d0a99a4c1a657f5965a
akshays-repo <akshayshan@gmail.com>
2020-08-01 19:25:00 +0530

    added chat bot

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,8 +1,7 @@
     window.__be = window.__be || {};
-    window.__be.id = "5f25351d74b9c20007c8259c";
+    window.__be.id = "5f2534d574b9c20007c82513";
     (function() {
         var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
         be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
     })();
-
